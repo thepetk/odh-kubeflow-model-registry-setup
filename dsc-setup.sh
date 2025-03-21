b05189ab9b7fbf3ff6ef898bb6be7afd84f27080
Gabe Montero <gmontero@redhat.com>
2025-03-20 22:42:25 -0400

    Update dsc-setup.sh rm patched dsc yaml ref

--- dsc-setup.sh
+++ dsc-setup.sh
@@ -1,5 +1,5 @@
 oc apply -f default-dsci.yaml
 oc wait --for=jsonpath='{.status.phase}'=Ready dsci/default-dsci --timeout=300s
 
-oc apply -f default-dsc-patched-for-kfmr.yaml
+oc apply -f default-dsc.yaml
 oc wait --for=jsonpath='{.status.phase}'=Ready dsc/default-dsc --timeout=300s
